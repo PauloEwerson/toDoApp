add09d52f061d5af1e13a153b7b1350da8c427b1
PauloEwerson <ewersonmrt@gmail.com>
2023-04-14 18:32:30 -0300

    refactor: Altera lógica aplica estilos

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, Text, FlatList, TouchableOpacity } from 'react-native';
 import { styles } from './styles';
-import Logo from '../components/Logo';
-import InputSection from '../components/InputSection';
 
-export default function Home() {
+interface Task {
+  key: string;
+  text: string;
+  completed: boolean;
+}
+
+const TodoList: React.FC = () => {
+  const [text, setText] = useState<string>('');
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [countCreated, setCountCreated] = useState<number>(0);
+  const [countCompleted, setCountCompleted] = useState<number>(0);
+
+  const handleAddTask = (text: string): void => {
+    setTasks([...tasks, { key: Date.now().toString(), text: text, completed: false }]);
+    setCountCreated(countCreated + 1);
+  };
+
+  const handleCompleteTask = (key: string): void => {
+    const updatedTasks = tasks.map((task) => {
+      if (task.key === key) {
+        task.completed = !task.completed;
+        setCountCompleted(task.completed ? countCompleted + 1 : countCompleted - 1);
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  };
+
   return (
     <View style={styles.container}>
-      <Logo />
-      <InputSection />
-      <StatusBar style="auto" />
+      {/* Logo */}
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Adicionar nova tarefa"
+          placeholderTextColor={'#999'}
+          onChangeText={(text) => setText(text)
+          }
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => handleAddTask(text)}
+        >
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <Text>Tarefas criadas: {countCreated}</Text>
+      <Text>Tarefas concluídas: {countCompleted}</Text>
+      <FlatList
+        data={tasks}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => handleCompleteTask(item.key)}>
+            <Text style={{ textDecorationLine: item.completed ? 'line-through' : 'none' }}>
+              {item.text}
+            </Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
-}
+};
+
+export default TodoList;
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -6,9 +6,34 @@ export const styles = StyleSheet.create({
     backgroundColor: '#131016',
     alignItems: 'center',
   },
-  containerInput: {
+  form: {
     flexDirection: 'row',
     width: '100%',
     padding: 20,
+    marginTop: 36,
+    marginBottom: 42,
+  },
+  input: {
+    flex: 1,
+    backgroundColor: '#1F1E25',
+    color: '#fff',
+    height: 56,
+    borderRadius: 5,
+    padding: 16,
+    fontSize: 16,
+    marginRight: 12,
+  },
+  button: {
+    height: 56,
+    width: 56,
+    borderRadius: 5,
+    backgroundColor: '#31CF67',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText : {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
   }
 });
